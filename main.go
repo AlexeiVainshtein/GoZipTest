16c57331b3f9e526b7d3095f8179075db8791700
Alexei Vainshtein <alexeiv@jfrog.com>
2018-12-17 15:01:09 +0200

    Update

--- main.go
+++ main.go
@@ -2,27 +2,120 @@ package main
 
 import (
 	"GoZipTest/Test"
+	"archive/zip"
 	"fmt"
-	"github.com/mholt/archiver"
+	"io"
+	"io/ioutil"
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
 )
+var compressedFormats = map[string]struct{}{
+	".7z":   {},
+	".avi":  {},
+	".bz2":  {},
+	".cab":  {},
+	".gif":  {},
+	".gz":   {},
+	".jar":  {},
+	".jpeg": {},
+	".jpg":  {},
+	".lz":   {},
+	".lzma": {},
+	".mov":  {},
+	".mp3":  {},
+	".mp4":  {},
+	".mpeg": {},
+	".mpg":  {},
+	".png":  {},
+	".rar":  {},
+	".tbz2": {},
+	".tgz":  {},
+	".txz":  {},
+	".xz":   {},
+	".zip":  {},
+	".zipx": {},
+}
 
 func main() {
-	wd, err := os.Getwd()
+	source, err := os.Getwd()
 	if err != nil {
 		return
 	}
-	Test.Print(wd)
+	Test.Print(source)
 	var list []string
-	filepath.Walk(wd, func(path string, info os.FileInfo, err error) error {
-		fmt.Println(path)
-		if !info.IsDir() {
-			return err
+	sourceInfo, err := os.Stat(source)
+	if err != nil {
+		panic(err)
+	}
+
+	var baseDir string
+	if sourceInfo.IsDir() {
+		baseDir = filepath.Base(source)
+	}
+	tempFile, err := ioutil.TempFile(source, "project.zip")
+	if err != nil {
+
+	}
+
+	w := zip.NewWriter(tempFile)
+
+	filepath.Walk(source, func(fpath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return fmt.Errorf("walking to %s: %v", fpath, err)
 		}
-		list = append(list, path)
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return fmt.Errorf("%s: getting header: %v", fpath, err)
+		}
+
+		if baseDir != "" {
+			name, err := filepath.Rel(source, fpath)
+			if err != nil {
+				return err
+			}
+			header.Name = path.Join(baseDir, filepath.ToSlash(name))
+		}
+
+		if info.IsDir() {
+			header.Name += "/"
+			header.Method = zip.Store
+		} else {
+			ext := strings.ToLower(path.Ext(header.Name))
+			if _, ok := compressedFormats[ext]; ok {
+				header.Method = zip.Store
+			} else {
+				header.Method = zip.Deflate
+			}
+		}
+
+		writer, err := w.CreateHeader(header)
+		if err != nil {
+			return fmt.Errorf("%s: making header: %v", fpath, err)
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		if header.Mode().IsRegular() {
+			file, err := os.Open(fpath)
+			if err != nil {
+				return fmt.Errorf("%s: opening: %v", fpath, err)
+			}
+			defer file.Close()
+
+			_, err = io.CopyN(writer, file, info.Size())
+			if err != nil && err != io.EOF {
+				return fmt.Errorf("%s: copying contents: %v", fpath, err)
+			}
+		}
+
 		return nil
 	})
-	fmt.Println(list)
-	archiver.Zip.Make("zipping.zip", []string{wd})
+	defer w.Close()
+	//fmt.Println(list)
+	//archiver.Zip.Make("zipping.zip", []string{source})
 }
